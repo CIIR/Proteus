fd3a1fe6db55e9777c6631d03c4694cfaa5facc4
Will Dabney <amarack@gmail.com>
2012-03-13 18:20:53 -0400

    Some small updates to workflow

--- workflow/scripts/clean_teis.sh
+++ workflow/scripts/clean_teis.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 f=`dirname $0`
-mkdir $f/../output/mbteis/cleaned
+mkdir $f/../$2
 
 # Call the program(s) to clean the fields of the TEI files in 'originals'
 echo "Clearing out old files in output/mbteis/cleaned/ ..."
-rm -rf $f/../output/mbteis/cleaned/*
+rm -rf $f/../${2}/*
 
 echo "Cleaning TEI fields..."
-# Don't have the programs yet so we are cheating:
-cp $f/../output/mbteis/originals/* $f/../output/mbteis/cleaned/
+# Move entity name's into the RS tags so that tag-tokenizer can create fields from them:
+#for fl in ${f}/../${1}/*_mbtei.xml.gz ; do sh ${f}/move_entity_tag.sh ${fl} ${f}/../${2} ; done
+cp ${f}/../${1}/*_mbtei.xml.gz ${f}/../${2}/
+
 
