71593b097fc23c05489fc4e21da99978f696ea83
Logan Giorda <logan.giorda@solutionset.com>
2012-07-16 21:39:51 -0700

    proteus thrift definition compiles, left in example HelloService thrift definition for guidance

--- aura/src/main/scala/ciir/aura/proteus/ProteusServiceImpl.scala
+++ aura/src/main/scala/ciir/aura/proteus/ProteusServiceImpl.scala
@@ -1,13 +1,13 @@
 package ciir.aura.proteus
 
-/**
- * Created with IntelliJ IDEA.
- * User: logan.giorda
- * Date: 7/16/12
- * Time: 9:07 PM
- * To change this template use File | Settings | File Templates.
- */
+import com.twitter.util.Future
 
-class ProteusServiceImpl {
+class ProteusServiceImpl extends ProteusProvider.FutureIface {
+
+  override def search(srequest: SearchRequest): Future[SearchResponse] = { }
+
+  override def lookup(lrequest: LookupRequest): Future[LookupResponse] = { }
+
+  override def transform(trequest: TransformRequest): Future[TransformResponse] = { }
 
 }
