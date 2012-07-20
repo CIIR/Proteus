7cf20910de6afc337d6bdcd3e23ba2c17be1ed8c
Marc Cartright <irmarc@cs.umass.edu>
2012-07-20 08:16:29 -0400

    Added the RemoteDataServer class, which will currently provide random data.

---
+++ aura/src/main/scala/ciir/proteus/app/RemoteDataServer.scala
@@ -0,0 +1,21 @@
+package ciir.proteus.app
+
+import ciir.proteus._
+import com.twitter.finagle.builder._
+import com.twitter.finagle.thrift.ThriftServerFramedCodec
+import org.apache.thrift.protocol._
+import java.net.InetSocketAddress
+
+object RemoteDataServer {
+  def main(args: Array[String]) {
+    val dataSource = new RemoteRandomDataProvider()
+    
+    val service = new ProteusProvider.FinagledService(dataSource, new TBinaryProtocol.Factory());
+
+    val server : Server = ServerBuilder()
+      .name("RandomProteusDataService")
+      .bindTo(new InetSocketAddress(8888))
+      .codec(ThriftServerFramedCodec())
+      .build(service)  // calls start underneath?
+  }
+}
