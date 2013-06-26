c93a208a9a4f555cfb4b7bc5bfaae85a6d8f0c17
John Foley <jfoley@cs.umass.edu>
2013-06-26 10:12:52 -0400

    main in homer; add slf4j as a dependency
    -- why doesn't maven pull this in automagically?

---
+++ homer/src/main/java/ciir/proteus/build/Main.java
@@ -0,0 +1,14 @@
+// BSD License (http://lemurproject.org/galago-license)
+package ciir.proteus.build;
+
+import org.lemurproject.galago.core.tools.App;
+
+/**
+ *
+ * @author jfoley
+ */
+public class Main {
+  public static void main(String[] args) throws Exception {
+    App.run(args);
+  }
+}
