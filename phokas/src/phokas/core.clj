785b087de2cb9c6adb31624676ab476d077775e5
David Smith <dasmiq@gmail.com>
2013-08-09 10:17:55 -0400

    Moved rawtei creation code to pontos.

--- phokas/src/phokas/core.clj
+++ phokas/src/phokas/core.clj
@@ -39,11 +39,6 @@
 
 (def ^:dynamic *languages* #{"eng" "fre" "ger" "ita" "lat"})
 
-(def ^:dynamic *lang-map* {"English" "eng",
-                           "French" "fre", "Français" "fre", "fra" "fre",
-                           "German" "ger",
-                           "Latin" "lat", "Italian" "ita", "Spanish" "spa"})
-
 (def ^:dynamic *dict* (-> "dict/words.eng.gz"
                           gzresource
                           line-seq set))
@@ -78,51 +73,6 @@
               ;;\\ "backslashesareinescapable"}
               \\ "\u2216"		; set minus
               })
-
-(defn dj-pages
-  [events]
-  (partition-when #(and (= (:name %) :MAP)
-			(= (:type %) :end-element))
-		  events))
-
-(defn dj-tags
-  [s]
-  (let [freqs (->> s
-		   (filter #(= (:type %) :characters))
-		   (map :str)
-		   (frequencies))
-	googles (or (freqs "Google") 0)]
-    (if (>= googles 3)
-      (let [page-start (first (filter #(and (= (:type %) :start-element)
-					    (= (:name %) :OBJECT))
-				      s))]
-	(str "<pb n=\""
-	     (nth (re-find #"_0*(\d+).djvu$" (:usemap (:attrs page-start))) 1)
-	     "\" />"))
-      (apply
-       str
-       (filter
-	not-empty
-	(map
-	 #(case (:type %)
-		:start-element
-		(case (:name %)
-		      :OBJECT (str "<pb n=\""
-				   (nth (re-find #"_0*(\d+).djvu$" ((:attrs %) :usemap)) 1)
-				   "\" />")
-		      :PAGECOLUMN "<cb />"
-		      :LINE "<lb />"
-		      :PARAGRAPH "<p>"
-		      :WORD (str "<w coords=\"" ((:attrs %) :coords) "\">")
-		      nil)
-		:end-element
-		(case (:name %)
-		      :LINE "\n"
-		      :PARAGRAPH "</p>\n"
-		      :WORD "</w> "
-		      nil)
-		:characters (s/escape (:str %) escapes))
-	 s))))))
 
 (defn no-tags
   [^String s]
@@ -291,15 +241,6 @@
 	      (xml-> :p :s))]
     para))
 
-(defn ^String dc-fix
-  [dc-string]
-  (-> dc-string
-      (s/replace
-       #"<language>([^<]+)</language>"
-       #(str "<language>"
-	     (if-let [lcode (*lang-map* (s/trim (% 1)))] lcode (% 1))
-	     "</language>"))))
-
 (defn dc-language
   [dc-string]
   (let [mt (zip/xml-zip (parse-str dc-string))]
@@ -422,38 +363,6 @@
 			   wspans heads deprels))
 	       (last wsegs))))))
 
-;; Before rearranging lines, e.g. headers and footers, we should
-;; probably number the lines.
-(defn proc-page
-  "Process one page. Tag headers (and footers) and move them
-  after (before) the next (previous) paragraph in sequence."
-  [^String p]
-  (-> p
-      (s/replace #"[ ]+\n" "\n")
-      (s/replace #"\n</p>" "</p>")
-      ;; Match up to 51 lines in the next (previous paragraph).
-      ;; Without these hard limits, the poor regex engine does a stack overflow.
-      (s/replace
-       #"^((?:<[^>]+>)*)<p>(.*)</p>\n(<p>(?:.*\n){0,50}?.*</p>)(\n<p>)"
-       "$1$3\n<fw place=\"top\">$2</fw>$4")
-      (s/replace
-       #"(</p>\n)(<p>(?:.*\n){0,50}?.*</p>\n)<p>(.*)</p>\n*$"
-       "$1<fw place=\"bottom\">$3</fw>\n$2")))
-
-(defn splice-page
-  "Given a triple of the previous, current, and next pages, figure out
-  whether the first and last paragraphs of the current page extend
-  beyond page boundaries."
-  [[prev cur next]]
-  (->> cur
-       ;; I guess we should make a conditional replace macro
-       (#(if (re-find #"[a-z][,:;\-]?</w></p>\n$" prev)
-	   (s/replace % #"^((?:<[^>]+>)*)<p>((?:<[^>]+>)*<w [^>]+>[a-z])" "$1$2")
-	   %))
-       (#(if (re-find #"^(?:<[^>]+>)*<p>(?:<[^>]+>)*<w [^>]+>[a-z]" next)
-	   (s/replace % #"([a-z][,:;\-]?</w>)</p>\n$" "$1\n")
-	   %))))
-
 (defn tei-words
   [s]
   (->> s
@@ -462,19 +371,6 @@
        (filter #(= (:type %) :characters))
        (map :str)
        (frequencies)))
-
-(defn dj-paras
-  "Convert a DjVuXML file into a lazy sequence of XML paragraphs with
-  tagged words."
-  [s]
-  (->> s
-      source-seq
-      dj-pages
-      (map dj-tags)
-      (map proc-page)
-      (lazy-cat [""])			; add leading empty page
-      (partition 3 1 [""])		; and trailing empty page
-      (map splice-page)))		; cat to string
 
 (defn tei-paras
   [s]
@@ -493,106 +389,6 @@
                  (s/replace #"</p>.*$" "")))))
   
   ;;(map word-forms (lazy-seq (s/split s #"</p>\n"))))
-
-(defn ocrml-sections
-  [events]
-  (partition-when #(and (= (:name %) :section)
-			(= (:type %) :end-element))
-		  events))
-
-(defn ocrml-tags
-  [s]
-  (let [label (:label (:attrs (first (filter #(and (= (:type %) :start-element) (= (:name %) :section)) s))))]
-    (apply
-     str
-     (filter
-      not-empty
-      (map
-       #(case (:type %)
-	      :start-element
-	      (let [attrs (:attrs %)]
-		(case (:name %)
-		      :section (case label
-				     "SEC_HEADER" "<fw place=\"top\">"
-				     "SEC_FOOTER" "<fw place=\"bottom\">"
-				     "<p>")
-		      :page (str "<pb n=\"" (:key attrs) "\" />")
-		      :line "<lb />"
-		      :word (str "<w coords=\"" (s/join "," (map (partial get attrs) [:l :t :w :h])) "\">" (s/escape (:val attrs) escapes))
-		      :marker (str "<marker" (map-str (fn [x] (str " " (name (first x)) "=\"" (second x) "\"")) attrs) " />")
-		      nil))
-	      :end-element
-	      (case (:name %)
-		    :section (case label
-				   "SEC_HEADER" "</fw>"
-				   "SEC_FOOTER" "</fw>"
-				   "</p>\n")
-		    :line "\n"
-		    :word "</w> "
-		    :page "<pb />"
-		    ;; :marker "</marker>"
-		    nil)
-	      :characters (s/escape (:str %) escapes))
-       s)))))
-
-(defn ocrml-page
-  [s]
-  (-> s
-      (s/replace #"[ ]+\n" "\n")
-      (s/replace #"\n</p>" "</p>")
-      (s/replace #"\n</fw>" "</fw>")
-      (s/replace #"(<fw [^>]+>.*</fw>)(<p>(?:.*\n){0,50}?.*</p>)(\n<p>)" "$2\n$1$3")
-  ))
-
-(defn ocrml-paras
-  "Transform OCRML markup into a sequence of TEI paragraphs."
-  [s]
-  (->> s
-       source-seq
-       ocrml-sections
-       (map-str ocrml-tags)
-       (#(s/split % #"<pb />"))
-       (lazy-seq)
-       (map ocrml-page)
-       (lazy-cat [""])			; add leading empty page
-       (partition 3 1 [""])		; and trailing empty page
-       (map splice-page)))		; cat to string
-
-(defn gut-forms
-  "Tag whitespace separated words in a Gutenberg file, ignoring
-  underbars used for italics."
-  [paridx text]
-  (str
-   "<p>"
-   (-> text
-       (s/escape escapes)
-       (s/replace #"\$" "&dollar;")
-       (partition-str #"[ \n_]+")
-       (#(map-indexed (fn [idx w]
-                        (if (re-find #"[^ \n_]" w)
-                          (str "<w coords=\"0,0," paridx "," (/ idx 2) "\">" w "</w>")
-                          w)) %))
-       (#(apply str %))
-       ;;(s/replace #" _" " <hi>")
-       ;;(s/replace #"_ " "</hi> ")
-       (s/replace #"&dollar;" "\\$"))
-   "</p>\n"))
-
-(defn gut-paras
-  "Turn a Gutenberg file into a lazy sequence of XML paragraphs with
-  tagged words."
-  [s]
-  (->> s
-       line-seq
-       (partition-by #(= % ""))
-       (map #(s/join "\n" %))
-       ;;(drop-while #(empty? (re-find #"START OF THE PROJECT GUTENBERG EBOOK" %)))
-       ;;(drop 1)
-       (drop-while #(re-find #"^\n" %))
-       ;;(take-while #(empty? (re-find #"END OF THE PROJECT GUTENBERG EBOOK" %)))
-       (map-indexed gut-forms)
-       (map-indexed #(if (re-find #"^<p>\n+</p>$" %2) (str "<pb n=\"" %1 "\" />") %2))
-       ))
 
 (defn ^String annotate-para
   "Perform linguistic annotation on a paragraph."
@@ -701,22 +497,6 @@
 	       toks)]))]
     (assoc props "unk" (- 1 (reduce + (vals props))))))
 
-(defn raw-file
-  [para-seq mpath ipath opath]
-  (println opath)
-  (let [metadata (-> mpath bzreader slurp (s/replace #"^<\?xml [^>]+\?>\n*" "") dc-fix)
-        encoding (if (re-find #"-8\." ipath) "ISO-8859-1" "UTF-8")
-        file-read #(jio/reader (if (re-find #"\.bz2$" %) (bzreader %) %) :encoding encoding)]
-    (with-open [in ^BufferedReader (file-read ipath)
-                out (-> opath java.io.FileOutputStream. GZIPOutputStream.
-                        (jio/writer :encoding "UTF-8"))]
-      (.write out "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<TEI>\n")
-      (.write out metadata)
-      (.write out "<text>\n")
-      (doseq [para (para-seq in)] (.write out para))
-      (.write out "\n</text></TEI>\n")
-      opath)))
-
 (defn nlp-annotate-file
   [ipath opath]
   (println opath)
@@ -756,18 +536,12 @@
           ifile (File. ipath)
           idir (.getParent ifile)
           bid (.getName (File. idir))
-          mpath (jio/file idir (str bid "_meta.xml.bz2"))
           raw-path (.getPath (jio/file idir (str bid ".rawtei.gz")))
           [call opath]
           (cond
            (re-find #".rawtei.gz$" ipath)
            [nlp-annotate-file (.getPath (jio/file idir (str bid ".mbtei.gz")))]
-           (re-find #"gut$" bid)
-           [(partial raw-file gut-paras mpath) raw-path]
-           (re-find #"_ocrml.xml$" ipath)
-           [(partial raw-file ocrml-paras mpath) raw-path]
-           (re-find #"_djvu.xml.bz2" ipath)
-           [(partial raw-file dj-paras mpath) raw-path])]
+           )]
       (try
         (call ipath opath)
         (catch Exception e
---
+++ pontos/project.clj
@@ -0,0 +1,11 @@
+(defproject pontos "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [org.clojure/data.zip "0.1.1"]
+		 [ciir/utils "1.0.0-SNAPSHOT"]
+                 ]
+  :main pontos.core)
