a03757194d26004fc51803c8ee8fc1bed91721a8
David Smith <dasmiq@gmail.com>
2013-08-12 14:43:25 -0400

    Avoid tiered compilation with new leiningen.

--- phokas/project.clj
+++ phokas/project.clj
@@ -1,6 +1,6 @@
 (defproject phokas "1.0.0-SNAPSHOT"
   :description "Process Internet Archive books"
-  :jvm-opts ["-Dfile.encoding=UTF-8" "-Xmx4g"]
+  :jvm-opts ^:replace ["-Dfile.encoding=UTF-8" "-Xmx4g"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.zip "0.1.1"]
