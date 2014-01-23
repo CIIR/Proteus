f5294426e8a9e0bb8cf8d1bec7553cd5dd16461d
John Foley <jfoley@cs.umass.edu>
2014-01-23 16:56:14 -0500

    Hack to use full path in classpath

--- homer/src/main/java/ciir/proteus/build/Main.java
+++ homer/src/main/java/ciir/proteus/build/Main.java
@@ -3,6 +3,7 @@ package ciir.proteus.build;
 import ciir.proteus.jobs.BuildEntityCorpus;
 import ciir.proteus.jobs.BuildPictureStore;
 import ciir.proteus.jobs.BuildWordDateIndex;
+import java.io.File;
 import org.lemurproject.galago.core.tools.App;
 import org.lemurproject.galago.core.tools.AppFunction;
 
@@ -15,6 +16,23 @@ public class Main {
     App.appFunctions.put(fn.getName(), fn);
   }
   public static void main(String[] args) throws Exception {
+    System.out.println("java.class.path: "+System.getProperty("java.class.path"));
+    
+    // hack class path to use absolute & canonical values
+    // split(":").map(toCanonical).join(":")
+    String pathSep = System.getProperty("path.separator");
+    String originalClassPath = System.getProperty("java.class.path");
+    String[] originalClassPathEntries = originalClassPath.split(pathSep);
+    
+    StringBuilder cpb = new StringBuilder();
+    for(int i=0; i<originalClassPathEntries.length; i++) {
+      if(i != 0) cpb.append(pathSep);
+      cpb.append((new File(originalClassPathEntries[i])).getCanonicalPath());
+    }
+    System.setProperty("java.class.path", cpb.toString());
+    System.out.println("java.class.path: "+System.getProperty("java.class.path"));
+
+    
     addFunction(new BuildEntityCorpus());
     addFunction(new BuildPictureStore());
     addFunction(new BuildWordDateIndex());
