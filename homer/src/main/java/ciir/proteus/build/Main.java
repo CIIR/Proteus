933d2e415c411a0bd8d68c767cc2b2a9d340f95b
John Foley <jfoley@cs.umass.edu>
2014-01-23 11:50:08 -0500

    Add a main class

---
+++ homer/src/main/java/ciir/proteus/build/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ciir.proteus.build;
+
+import org.lemurproject.galago.core.tools.App;
+
+/**
+ *
+ * @author jfoley
+ */
+public class Main {
+  public static void main(String[] args) throws Exception {
+    App.main(args);
+  }
+}
