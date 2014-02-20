6b191710ee9a4fda968cbe608087d63c8ac990d8
John Foley <jfoley@cs.umass.edu>
2014-02-20 11:16:03 -0500

    Nuclear delete of unused types and code.

--- homer/src/main/java/ciir/proteus/build/Main.java
+++ homer/src/main/java/ciir/proteus/build/Main.java
@@ -1,26 +1,14 @@
 package ciir.proteus.build;
 
-import ciir.proteus.jobs.BuildEntityCorpus;
-import ciir.proteus.jobs.BuildPictureStore;
-import ciir.proteus.jobs.BuildWordDateIndex;
-import java.io.File;
 import org.lemurproject.galago.core.tools.App;
-import org.lemurproject.galago.core.tools.AppFunction;
 
 /**
- *
+ * Pass through to Galago.
  * @author jfoley
  */
 public class Main {
-  private static void addFunction(AppFunction fn) {
-    App.appFunctions.put(fn.getName(), fn);
-  }
   public static void main(String[] args) throws Exception {
     System.out.println("java.class.path: "+System.getProperty("java.class.path"));
-    
-    addFunction(new BuildEntityCorpus());
-    addFunction(new BuildPictureStore());
-    addFunction(new BuildWordDateIndex());
     App.run(args);
   }
 }
