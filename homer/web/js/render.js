4ed77d0a58cbe229397bae3d2a50055ae38d9f44
mzarozinski <MichaelZ@cs.umass.edu>
2014-05-01 16:17:56 -0400

    small fixes to only show a tag box where there are read-only tags, and to give you a box to type in tags if you haven't tagged anything yet.

--- homer/web/js/render.js
+++ homer/web/js/render.js
@@ -32,29 +32,41 @@ resultRenderers["default"] = function(queryTerms, result) {
 //html += '<td class="meta" colspan="2">TODO: default meta</td>'; // single column
   }
   html += '</tr></table><div>';
+
   if (result.tags) {
-    for (user in result.tags) {
-      tags = result.tags[user].toString().split(',');
-      // if these are OUR tags, we can edit
-      if (getCookie("username") == user) {
-        html += '<ul class="tags_' + result.name + '">  ';
+    // we ALWAYS want a div if you're logged in so you can add tags
+    var username = getCookie("username");
+    if (username !== "") {
+
+      html += '<ul class="tags_' + result.name + '">  ';
+      if (typeof result.tags[username] !== 'undefined') {
+        tags = result.tags[username].toString().split(',');
         for (tag in tags) {
           html += '  <li> ' + tags[tag] + ' </li> ';
         }
-        html += '</ul>'
-      } else {
-        // read-only tags
-        html += '<ul class="read-only-tags">';
-        for (tag in tags) {
-          html += '  <li> ' + tags[tag] + ' </li> ';
-        }
-        html += '</ul>';
       }
+      html += '</ul>'
+    } // end if someone is logged in
 
+    var tmp_html = "";
+    var read_only_tags = false;
+    for (user in result.tags) {
 
+      tags = result.tags[user].toString().split(',');
+      // skip current user
+      if (user !== username) {
+        for (tag in tags) {
+          read_only_tags = true;
+          tmp_html += '  <li> ' + tags[tag] + ' </li> ';
+        }
+      }
+    }
+    // only show the tag box if there is something to show
+    if (read_only_tags === true) {
+      html += '<ul class="read-only-tags">' + tmp_html + '</ul>'
     }
 
-  }
+  } // end if display tags
 
   html += '</div>';
   return html;
