acc29c92c37f17665f32a7f435c8e60bf9fcb49d
John Foley <jfoley@cs.umass.edu>
2014-05-16 01:45:02 -0400

    Remove separate test files for same thing...

--- homer/src/test/java/ciir/proteus/parse/MBTEIPageParserTest.java
+++ homer/src/test/java/ciir/proteus/parse/MBTEIPageParserTest.java
@@ -2,12 +2,50 @@ package ciir.proteus.parse;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.lemurproject.galago.core.parse.Document;
 import org.lemurproject.galago.core.types.DocumentSplit;
+import org.lemurproject.galago.tupleflow.FileUtility;
 import org.lemurproject.galago.tupleflow.Parameters;
+import org.lemurproject.galago.tupleflow.Utility;
 
+import javax.xml.stream.XMLStreamException;
+import java.io.File;
 import java.io.IOException;
 
 public class MBTEIPageParserTest {
+  @Test
+  public void testPage() throws IOException, XMLStreamException {
+    String data = "<TEI>" +
+      "<identifier>foo</identifier>"+
+      "<text>"+
+      "<w form=\"first\">firstWord</w>"+
+      "<pb n=\"0\" />"+
+      "<w form=\"second\">huh</w>"+
+      "<pb n=\"1\" />"+
+      "</text>"+
+      "</TEI>";
+
+    File tmp = FileUtility.createTemporary();
+    Utility.copyStringToFile(data, tmp);
+
+    DocumentSplit split = new DocumentSplit();
+    split.fileName = tmp.getAbsolutePath();
+    split.fileType = "mbtei";
+    MBTEIPageParser parser = new MBTEIPageParser(split, new Parameters());
+
+    Document page1 = parser.nextDocument();
+    Assert.assertNotNull(page1);
+    Assert.assertEquals("0", page1.metadata.get("pageNumber"));
+    Assert.assertEquals("first", page1.text.trim());
+
+    Document page2 = parser.nextDocument();
+    Assert.assertNotNull(page2);
+    Assert.assertEquals("1", page2.metadata.get("pageNumber"));
+    Assert.assertEquals("second", page2.text.trim());
+
+    Assert.assertNull(parser.nextDocument());
+  }
+
   @Test
   public void testParseCrapDocument() throws IOException {
     DocumentSplit input = new DocumentSplit();
