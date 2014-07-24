66565de0b38494bf961366f68eb613c376b3aa4b
Benjamin Zifkin <bzifkin@dampier.cs.umass.edu>
2014-07-24 14:55:54 -0400

    moved around a lot of code to make main shorter, no longer write line and paragraph tags since i figure their useless

--- homer/src/main/java/ciir/proteus/parse/XMLTrimmer.java
+++ homer/src/main/java/ciir/proteus/parse/XMLTrimmer.java
@@ -1,3 +1,4 @@
+//package ciir.proteus.parse;
 
 import java.io.File;
 
@@ -33,6 +34,8 @@ import javax.xml.stream.events.XMLEvent;
  */
 public class XMLTrimmer {
 
+    File readFile;
+    File writeFile;
     int marginUp = 0; //these are the margins which determines if a token is eligible or not
     int marginDown = 0;
     int marginLeft = 0;
@@ -41,154 +44,58 @@ public class XMLTrimmer {
     int pageHeight = 0;
     int dpi = 0;
     int quarterInch = 0;
-    boolean goodWord = false;
+    int xOne = 0; //coordinates of the words
+    int yOne = 0;
+    int xTwo = 0;
+    int yTwo = 0;
+    XMLEventReader reader = null;
+    XMLEventWriter writer = null;
+    XMLEventFactory eventFactory = null;
 
-    public static void main(String args[]) throws IOException, XMLStreamException {
-        int xOne = 0;
-        int yOne = 0;
-        int xTwo = 0;
-        int yTwo = 0;
-
-        XMLTrimmer xr = new XMLTrimmer();
-
-        XMLInputFactory factory = XMLInputFactory.newInstance();  //initiate readers/writers/factories
-        XMLOutputFactory outputFactory = XMLOutputFactory.newFactory();
-
-        XMLEventWriter writer = null;
+    public XMLTrimmer(XMLInputFactory ipf, XMLOutputFactory opf, File rfile, File wfile) throws FileNotFoundException, XMLStreamException {
+        readFile = rfile;
+        writeFile = wfile;
         try {
-            writer = outputFactory
-                    .createXMLEventWriter(new FileOutputStream(args[1]), "UTF-8");
-
-        } catch (XMLStreamException ex) {
-            Logger.getLogger(XMLTrimmer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
-        StartDocument startDocument = eventFactory.createStartDocument();
-
-        writer.add(startDocument);
-        XMLEventReader reader = null; //intializing the eventreader
-
-        try { //main loop with logic, continues as long as reader has a next event
-            //two main if loops in here, one looking for end elemnets the other start elements
-            reader = factory.createXMLEventReader(new FileInputStream(args[0]));
-
-            XMLEvent event = null;
-
-            while (reader.hasNext()) {
-
-                event = reader.nextEvent();
-
-                if (event.isStartElement()) { //first it looks for start elements
-                    StartElement se = event.asStartElement();
-                    if ("OBJECT".equals(se.getName().getLocalPart())) {
-
-                        Iterator<Attribute> attributes = se.getAttributes();
-                        while (attributes.hasNext()) {
-
-                            Attribute attribute = attributes.next();
-                            //grabs some attributes set the value to variables
-
-                            if (attribute.getName().toString().equals("height")) {
-                                xr.pageHeight = Integer.valueOf(attribute.getValue());
-                            }
-
-                            if (attribute.getName().toString().equals("width")) {
-                                xr.pageWidth = Integer.valueOf(attribute.getValue());
-                            }
-                        }
-                        // System.out.println("<OBJECT>");
-                        xr.calculateMargins(xr.pageHeight, xr.pageWidth); //how to make sure this isnt called everytime
-
-                    } else if ("MAP".equals(se.getName().getLocalPart())) {
-                        writer.add(se);
-
-                    } else if ("BODY".equals(se.getName().getLocalPart())) {
-                        writer.add(se);
-
-                    } else if ("PARAM".equals(se.getName().getLocalPart())) {
-
-                        Iterator<Attribute> attributes = se.getAttributes();
-                        while (attributes.hasNext()) {
-
-                            Attribute attribute = attributes.next();
-
-                            if (attribute.getValue().toString().equals("DPI")) {
-                                
-                               
-                                    xr.dpi = Integer.valueOf(attributes.next().getValue());
-                                }
-                                xr.quarterInch = (int) (xr.dpi * .25);
-
-                            }
-
-                        
-                    } else if ("PARAGRAPH".equals(se.getName().getLocalPart())) {
-                        //System.out.println("<PARAGRAPH>");
-                        writer.add(se);
-
-                    } else if ("LINE".equals(se.getName().getLocalPart())) {
-
-                        //System.out.println("<LINE>");
-                        writer.add(se);
-
-                    } else if ("WORD".equals(se.getName().getLocalPart())) {
-
-                        Iterator<Attribute> coord = se.getAttributes();
-                        Attribute position = coord.next();
-
-                        if (position.getName().toString().equals("coords")) {//add else
-                            StringTokenizer st = new StringTokenizer(position.getValue(), ",");
-
-                            xOne = Integer.valueOf(st.nextToken());
-                            yOne = Integer.valueOf(st.nextToken());
-                            xTwo = Integer.valueOf(st.nextToken());
-                            yTwo = Integer.valueOf(st.nextToken());
-
-                        }
-
-                        if (xr.inMargin2(xOne, yOne, xTwo, yTwo)) { //check to see if in margins, if it is the end tag is written immediately, otherwise nothing is written
-                            writer.add(se);
-                            EndElement wordEnd = eventFactory.createEndElement("", "", "WORD");
-                            Characters characters = eventFactory.createCharacters(reader.getElementText());
-                            writer.add(characters);
-                            writer.add(wordEnd);
-                        }
-
-                    }
-
-                } else if (event.isEndElement()) {
-
-                    EndElement ee = event.asEndElement();
-                    if ("LINE".equals(ee.getName().getLocalPart())) {
-                        writer.add(ee);
-
-                    } else if ("PARAGRAPH".equals(ee.getName().getLocalPart())) {
-                        writer.add(ee);
-
-                    } else if ("MAP".equals(ee.getName().getLocalPart())) {
-                        writer.add(ee);
-
-                    } else if ("BODY".equals(ee.getName().getLocalPart())) {
-                        writer.add(ee);
-                    }
-                }
-            }
-
+            reader = ipf.createXMLEventReader(new FileInputStream(readFile));
         } catch (FileNotFoundException e) {
             System.out.println("Could not find the file. Try again.");
         } catch (XMLStreamException e) {
             e.printStackTrace();
             System.out.println("There was an XML Stream Exception, whatever that means");
-        } finally {
-            writer.flush();
-            writer.close();
         }
-        xr.showMargins();
 
+        try {
+            writer = opf
+                    .createXMLEventWriter(new FileOutputStream(writeFile), "UTF-8");
+
+        } catch (XMLStreamException ex) {
+            Logger.getLogger(XMLTrimmer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        eventFactory = XMLEventFactory.newInstance();
+        StartDocument startDocument = eventFactory.createStartDocument();
+
+        writer.add(startDocument);
     }
-//calculate margins to be outer 10%
 
+    public void setPageHeightAndWidth(StartElement se) {
+        Iterator<Attribute> attributes = se.getAttributes();
+        while (attributes.hasNext()) {
+
+            Attribute attribute = attributes.next();
+            //grabs some attributes set the value to variables
+
+            if (attribute.getName().toString().equals("height")) {
+                pageHeight = Integer.valueOf(attribute.getValue());
+            }
+
+            if (attribute.getName().toString().equals("width")) {
+                pageWidth = Integer.valueOf(attribute.getValue());
+            }
+        }
+    }
+
+    //calculate margins to be outer 10%
     public void calculateMargins(int ph, int pw) {
         marginUp = (int) (ph * .1);
         marginDown = pageHeight - ((int) (ph * .1));
@@ -196,24 +103,107 @@ public class XMLTrimmer {
         marginRight = pageWidth - ((int) (pw * .1));
 
     }
+//this grabs the amount of pixels in an inch, allowing us to add a 
+// quarter inch of wiggle room to the margins to account for
+// noise, uneven scanning, etc
+    public void setDPI(StartElement se) {
+        Iterator<Attribute> attributes = se.getAttributes();
+        while (attributes.hasNext()) {
 
-   /* public void showMargins() {
-        System.out.println("margin up: " + (marginUp + quarterInch));
-        System.out.println("margin down: " + (marginDown - quarterInch));
-        System.out.println("margin right: " + (marginRight - quarterInch));
-        System.out.println("margin left: " + (marginLeft + quarterInch));
-        System.out.println("qi: " + quarterInch);
-    }*/
+            Attribute attribute = attributes.next();
+
+            if (attribute.getValue().toString().equals("DPI")) {
+
+                dpi = Integer.valueOf(attributes.next().getValue());
+            }
+            quarterInch = (int) (dpi * .25);
+
+        }
+
+    }
+
+    public void getWordCoords(StartElement se) {
+        Iterator<Attribute> coord = se.getAttributes();
+        Attribute position = coord.next();
+
+        
+            StringTokenizer st = new StringTokenizer(position.getValue(), ",");
+
+            xOne = Integer.valueOf(st.nextToken());
+            yOne = Integer.valueOf(st.nextToken());
+            xTwo = Integer.valueOf(st.nextToken());
+            yTwo = Integer.valueOf(st.nextToken());
+
+        
+    }
 //see if in margins
 
-    public boolean inMargin2(int xone, int yone, int xtwo, int ytwo) {
+    public boolean inMargin(int xone, int yone, int xtwo, int ytwo) {
         if (xone <= (marginLeft + quarterInch) || yone >= (marginDown - quarterInch) || xtwo >= (marginRight - quarterInch) || ytwo <= (marginUp + quarterInch)) {
-            goodWord = true;
             return true;
         } else {
-            goodWord = false;
             return false;
         }
+    }
+
+    public static void main(String args[]) throws IOException, XMLStreamException {
+        XMLInputFactory factory = XMLInputFactory.newInstance();  //initiate readers/writers/factories
+        XMLOutputFactory outputFactory = XMLOutputFactory.newFactory();
+        File rfile = new File(args[0]);
+        File wfile = new File(args[1]);
+        XMLTrimmer xr = new XMLTrimmer(factory, outputFactory, rfile, wfile);;
+
+        XMLEvent event = null;
+
+        while (xr.reader.hasNext()) {
+
+            event = xr.reader.nextEvent();
+
+            if (event.isStartElement()) { //first it looks for start elements
+                StartElement se = event.asStartElement();
+                if ("OBJECT".equals(se.getName().getLocalPart())) {
+
+                    xr.setPageHeightAndWidth(se);
+
+                    xr.calculateMargins(xr.pageHeight, xr.pageWidth); //how to make sure this isnt called everytime
+
+                } else if ("MAP".equals(se.getName().getLocalPart())) {
+                    xr.writer.add(se);
+
+                } else if ("BODY".equals(se.getName().getLocalPart())) {
+                    xr.writer.add(se);
+
+                } else if ("PARAM".equals(se.getName().getLocalPart())) {
+
+                    xr.setDPI(se);
+
+                } else if ("WORD".equals(se.getName().getLocalPart())) {
+                    xr.getWordCoords(se);
+
+                    if (xr.inMargin(xr.xOne, xr.yOne, xr.xTwo, xr.yTwo)) { //check to see if in margins, if it is the end tag is written immediately, otherwise nothing is written
+                        xr.writer.add(se);
+                        EndElement wordEnd = xr.eventFactory.createEndElement("", "", "WORD");
+                        Characters characters = xr.eventFactory.createCharacters(xr.reader.getElementText());
+                        xr.writer.add(characters);
+                        xr.writer.add(wordEnd);
+                    }
+
+                }
+
+            } else if (event.isEndElement()) {
+
+                EndElement ee = event.asEndElement();
+                if ("MAP".equals(ee.getName().getLocalPart())) {
+                    xr.writer.add(ee);
+
+                } else if ("BODY".equals(ee.getName().getLocalPart())) {
+                    xr.writer.add(ee);
+                }
+            }
+        }
+        xr.writer.flush();
+        xr.writer.close();
+        //xr.showMargins();
     }
 
 }
