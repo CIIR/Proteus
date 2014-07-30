935e2e0d4e36eeede55c863393615ee47139df2d
mzarozinski <MichaelZ@cs.umass.edu>
2014-07-30 09:32:03 -0400

    updates to page number logic to match the image "page" at the Internet Archive.

--- homer/src/main/java/ciir/proteus/parse/MBTEIPageParser.java
+++ homer/src/main/java/ciir/proteus/parse/MBTEIPageParser.java
@@ -19,93 +19,114 @@ import java.util.logging.Logger;
  */
 public class MBTEIPageParser extends DocumentStreamParser {
 
-  public static final Logger log = Logger.getLogger(MBTEIPageParser.class.getName());
-  public static final XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
+    public static final Logger log = Logger.getLogger(MBTEIPageParser.class.getName());
+    public static final XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
 
-  static {
-    xmlFactory.setProperty(XMLInputFactory.IS_COALESCING, true);
-  }
-  private XMLStreamReader xml;
-  private final DocumentSplit split;
-  private Map<String,String> metadata;
-  public int pageIndex = 0;
-
-  public MBTEIPageParser(DocumentSplit split, Parameters p) throws IOException {
-    super(split, p);
-
-    this.split = split;
-    metadata = null;
-    this.xml = null;
-    try {
-      this.xml = xmlFactory.createXMLStreamReader(getBufferedInputStream(split));
-    } catch (XMLStreamException e) {
-      log.log(Level.WARNING, "Failed to create page parser for split="+split.fileName+" ",e);
-    } catch (IOException e) {
-      log.log(Level.WARNING, "Failed to create page parser for split="+split.fileName+" ",e);
+    static {
+        xmlFactory.setProperty(XMLInputFactory.IS_COALESCING, true);
     }
-  }
+    private XMLStreamReader xml;
+    private final DocumentSplit split;
+    private Map<String, String> metadata;
+    public int pageIndex = -1;
 
-  @Override
-  public void close() throws IOException {
-    if(xml == null) return;
-    try {
-      xml.close();
-    } catch (XMLStreamException xml) {
-      throw new IOException(xml);
-    }
-  }
+    public MBTEIPageParser(DocumentSplit split, Parameters p) throws IOException {
+        super(split, p);
 
-  @Override
-  public Document nextDocument() {
-    if(xml == null) return null;
-    try {
-      if (metadata == null) {
-        metadata = MBTEI.parseMetadata(xml);
-      }
-      return nextPage();
-    } catch (XMLStreamException e) {
-      log.log(Level.WARNING, "XML Exception", e);
-      return null;
-    }
-  }
-
-  private Document nextPage() throws XMLStreamException {
-    if (!xml.hasNext()) {
-      return null;
-    }
-
-    // local parser state
-    StringBuilder buffer = new StringBuilder();
-    boolean empty = true;
-
-    while (xml.hasNext()) {
-      int event = xml.next();
-
-      // copy the "form" attribute out of word tags
-      if (event == XMLStreamConstants.START_ELEMENT) {
-        String tag = xml.getLocalName();
-        if ("w".equals(tag)) {
-          String formValue = MBTEI.scrub(xml.getAttributeValue(null, "form"));
-          if (!formValue.isEmpty()) {
-            buffer.append(formValue).append(' ');
-            empty = false;
-          }
-        } else if ("pb".equals(tag)) {
-          // echo a document when we find the <pb /> tag
-          if(!empty) break;
+        this.split = split;
+        metadata = null;
+        this.xml = null;
+        try {
+            this.xml = xmlFactory.createXMLStreamReader(getBufferedInputStream(split));
+        } catch (XMLStreamException e) {
+            log.log(Level.WARNING, "Failed to create page parser for split=" + split.fileName + " ", e);
+        } catch (IOException e) {
+            log.log(Level.WARNING, "Failed to create page parser for split=" + split.fileName + " ", e);
         }
-      }
     }
 
-    if(empty) return null;
+    @Override
+    public void close() throws IOException {
+        if (xml == null) {
+            return;
+        }
+        try {
+            xml.close();
+        } catch (XMLStreamException xml) {
+            throw new IOException(xml);
+        }
+    }
 
-    final String pageNumber = Integer.toString(pageIndex++);
-    Document page = new Document();
-    String archiveId = MBTEI.getArchiveIdentifier(split, metadata);
-    page.text = buffer.toString();
-    page.name = archiveId + "_" + pageNumber;
-    page.metadata = metadata;
-    page.metadata.put("pageNumber", pageNumber);
-    return page;
-  }
+    @Override
+    public Document nextDocument() {
+        if (xml == null) {
+            return null;
+        }
+        try {
+            if (metadata == null) {
+                metadata = MBTEI.parseMetadata(xml);
+            }
+            return nextPage();
+        } catch (XMLStreamException e) {
+            log.log(Level.WARNING, "XML Exception", e);
+            return null;
+        }
+    }
+
+    private Document nextPage() throws XMLStreamException {
+        if (!xml.hasNext()) {
+            return null;
+        }
+
+        // local parser state
+        StringBuilder buffer = new StringBuilder();
+        boolean empty = true;
+
+        while (xml.hasNext()) {
+            int event = xml.next();
+
+            // since the books do NOT end with <pb> we need to "fake" that
+            // so the last page number works
+            if (event == XMLStreamConstants.END_ELEMENT) {
+                String tag = xml.getLocalName();
+                if ("text".equals(tag)) {
+                    pageIndex++;
+                    break; // we're done
+                }
+            }
+            // copy the "form" attribute out of word tags
+            if (event == XMLStreamConstants.START_ELEMENT) {
+                String tag = xml.getLocalName();
+                if ("w".equals(tag)) {
+                    String formValue = MBTEI.scrub(xml.getAttributeValue(null, "form"));
+                    if (!formValue.isEmpty()) {
+                        buffer.append(formValue).append(' ');
+                        empty = false;
+                    }
+                } else if ("pb".equals(tag)) {
+
+                    pageIndex++;
+                    if (!empty) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (empty) {
+            return null;
+        }
+
+        // NOTE - we subtract 1 from th page number. the <pb> tags START
+        // a page, but with the logic above, we hit the <pb> that starts the 
+        // NEXT page so we're one too many.
+        final String pageNumber = Integer.toString(pageIndex - 1);
+        Document page = new Document();
+        String archiveId = MBTEI.getArchiveIdentifier(split, metadata);
+        page.text = buffer.toString();
+        page.name = archiveId + "_" + pageNumber;
+        page.metadata = metadata;
+        page.metadata.put("pageNumber", pageNumber);
+        return page;
+    }
 }
--- homer/src/test/java/ciir/proteus/parse/MBTEIPageParserTest.java
+++ homer/src/test/java/ciir/proteus/parse/MBTEIPageParserTest.java
@@ -14,56 +14,105 @@ import java.io.File;
 import java.io.IOException;
 
 public class MBTEIPageParserTest {
-  @Test
-  public void testPage() throws IOException, XMLStreamException {
-    String data = "<TEI>" +
-      "<identifier>foo</identifier>"+
-      "<text>"+
-      "<w form=\"first\">firstWord</w>"+
-      "<pb n=\"0\" />"+
-      "<w form=\"second\">huh</w>"+
-      "<pb n=\"1\" />"+
-      "</text>"+
-      "</TEI>";
 
-    File tmp = FileUtility.createTemporary();
-    Utility.copyStringToFile(data, tmp);
+    @Test
+    public void testPage() throws IOException, XMLStreamException {
+        String data = "<TEI>"
+                + "<identifier>foo</identifier>"
+                + "<text>"
+                + "<pb n=\"0\" />"
+                + "<w form=\"first\">firstWord</w>"
+                + "<pb n=\"1\" />"
+                + "<w form=\"second\">huh</w>"
+                + "</text>"
+                + "</TEI>";
 
-    DocumentSplit split = DocumentSplitFactory.file(tmp, "mbtei");
-    MBTEIPageParser parser = new MBTEIPageParser(split, Parameters.instance());
+        File tmp = FileUtility.createTemporary();
+        Utility.copyStringToFile(data, tmp);
 
-    Document page1 = parser.nextDocument();
-    Assert.assertNotNull(page1);
-    Assert.assertEquals("0", page1.metadata.get("pageNumber"));
-    Assert.assertEquals("first", page1.text.trim());
+        DocumentSplit split = DocumentSplitFactory.file(tmp, "mbtei");
+        MBTEIPageParser parser = new MBTEIPageParser(split, Parameters.instance());
 
-    Document page2 = parser.nextDocument();
-    Assert.assertNotNull(page2);
-    Assert.assertEquals("1", page2.metadata.get("pageNumber"));
-    Assert.assertEquals("second", page2.text.trim());
+        Document page1 = parser.nextDocument();
+        Assert.assertNotNull(page1);
+        Assert.assertEquals("0", page1.metadata.get("pageNumber"));
+        Assert.assertEquals("first", page1.text.trim());
 
-    Assert.assertNull(parser.nextDocument());
-  }
+        Document page2 = parser.nextDocument();
+        Assert.assertNotNull(page2);
+        Assert.assertEquals("1", page2.metadata.get("pageNumber"));
+        Assert.assertEquals("second", page2.text.trim());
 
-  @Test
-  public void testParseCrapDocument() throws IOException {
-    DocumentSplit input = DocumentSplitFactory.file("src/test/resources/EmptyFile");
-    MBTEIPageParser pages = new MBTEIPageParser(input, Parameters.instance());
-    Assert.assertNotNull(pages);
-    Assert.assertNull(pages.nextDocument());
-    pages.close();
+        Assert.assertNull(parser.nextDocument());
+    }
 
-    input.fileName = ".gitignore";
-    pages = new MBTEIPageParser(input, Parameters.instance());
-    Assert.assertNotNull(pages);
-    Assert.assertNull(pages.nextDocument());
-    pages.close();
+    @Test
+    public void testPage2() throws IOException, XMLStreamException {
+        String data = "<TEI>"
+                + "<metadata>"
+                + "	<identifier>test</identifier>"
+                + "	<title>test title</title>"
+                + "</metadata>"
+                + "<text lang=\"eng\">"
+                + "	<pb n=\"1\"/>"
+                + "	<w form=\"page one\" >page one</w>"
+                + "	<pb n=\"2\"/>"
+                + "	<w  form=\"page two\" >page two</w>"
+                + "	<pb n=\"3\"/>"
+                + "	<pb n=\"4\"/>"
+                + "	<pb n=\"5\"/>"
+                + "	<pb n=\"6\"/>"
+                + "	<pb n=\"7\"/>"
+                + "	<w  form=\"page seven\" >page seven</w>"
+                + "</text>"
+                + "</TEI>";
 
-    input.fileName = "src/test/resources/nearly-empty.mbtei.gz";
-    pages = new MBTEIPageParser(input, Parameters.instance());
-    Assert.assertNotNull(pages);
-    Assert.assertNull(pages.nextDocument());
-    pages.close();
-  }
+        File tmp = FileUtility.createTemporary();
+        Utility.copyStringToFile(data, tmp);
+
+        DocumentSplit split = DocumentSplitFactory.file(tmp, "mbtei");
+        MBTEIPageParser parser = new MBTEIPageParser(split, Parameters.instance());
+        {
+            Document page = parser.nextDocument();
+            Assert.assertNotNull(page);
+            Assert.assertEquals("0", page.metadata.get("pageNumber"));
+            Assert.assertEquals("page one", page.text.trim());
+        }
+        {
+            Document page = parser.nextDocument();
+            Assert.assertNotNull(page);
+            Assert.assertEquals("1", page.metadata.get("pageNumber"));
+            Assert.assertEquals("page two", page.text.trim());
+        }
+
+        {
+            Document page = parser.nextDocument();
+            Assert.assertNotNull(page);
+            Assert.assertEquals("6", page.metadata.get("pageNumber"));
+            Assert.assertEquals("page seven", page.text.trim());
+        }
+        Assert.assertNull(parser.nextDocument());
+    }
+
+    @Test
+    public void testParseCrapDocument() throws IOException {
+        DocumentSplit input = DocumentSplitFactory.file("src/test/resources/EmptyFile");
+        MBTEIPageParser pages = new MBTEIPageParser(input, Parameters.instance());
+        Assert.assertNotNull(pages);
+        Assert.assertNull(pages.nextDocument());
+        pages.close();
+
+        input.fileName = ".gitignore";
+        pages = new MBTEIPageParser(input, Parameters.instance());
+        Assert.assertNotNull(pages);
+        Assert.assertNull(pages.nextDocument());
+        pages.close();
+
+        input.fileName = "src/test/resources/nearly-empty.mbtei.gz";
+        pages = new MBTEIPageParser(input, Parameters.instance());
+        Assert.assertNotNull(pages);
+        Assert.assertNull(pages.nextDocument());
+        pages.close();
+    }
 
 }
