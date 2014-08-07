fa27ef7c79c1c85b545e96272566cf20044824b5
Benjamin Zifkin <bzifkin@dampier.cs.umass.edu>
2014-08-07 16:30:25 -0400

    UPDATED: now can read in XML/DjVU input, trim it, and then recognize and occasionally extrapolate page numbers that fit into either an arabic or roman numbering scheme

--- homer/src/main/java/ciir/proteus/parse/XMLTrimmer.java
+++ homer/src/main/java/ciir/proteus/parse/XMLTrimmer.java
@@ -1,5 +1,6 @@
-//package ciir.proteus.parse;
+package ciir.proteus.parse;
 
+//import ciir.proteus.parse.Pages.Word;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,8 +14,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLEventWriter;
@@ -22,9 +21,6 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.Characters;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartDocument;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -48,8 +44,6 @@ public class XMLTrimmer {
     int yOne = 0;
     int xTwo = 0;
     int yTwo = 0;
-
-    boolean knowPageHeightWidth = false;
 
     static XMLInputFactory factory = XMLInputFactory.newInstance();  //initiate readers/writers/factories
     static XMLOutputFactory outputFactory = XMLOutputFactory.newFactory();
@@ -91,8 +85,8 @@ public class XMLTrimmer {
                 pWidth = Integer.valueOf(attribute.getValue());
             }
         }
-        System.out.println("pHeigt: " + pHeight);
-        System.out.println("pWidth : " + pWidth);
+        //System.out.println("pHeigt: " + pHeight);
+        //System.out.println("pWidth : " + pWidth);
         calculateMargins(pHeight, pWidth);
 
     }
@@ -118,13 +112,12 @@ public class XMLTrimmer {
     //calculate margins to be outer 10%
     public void calculateMargins(int ph, int pw) {
         marginUp = (int) (ph * .1);
-        
+
         marginDown = ph - ((int) (ph * .1));
-        
+
         marginLeft = (int) (pw * .1);
-        
+
         marginRight = pw - ((int) (pw * .1));
-        
 
     }
 //this grabs the amount of pixels in an inch, allowing us to add a 
@@ -155,7 +148,93 @@ public class XMLTrimmer {
         }
     }
 
-    public List<Pages> run(XMLEvent event) throws XMLStreamException {
+    public boolean isArabicNum(String text) {
+        String bs = text;
+
+        try {
+            Integer.parseInt(bs);
+
+        } catch (NumberFormatException e) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /*this method doesnt work quite right
+     it will appropriately recognizes well formatted
+     roman numerals, but will also recognize 
+     incorrectly formatted numerals
+     for example it interperts "iix" as 10.
+     However, in the context it's being used I
+     don't think this will cause problems
+     */
+    public static int romanToDecimal(String number) {
+        int decimal = 0;
+        int lastNumber = 0;
+        String romanNumeral = number.toUpperCase();
+        /* operation to be performed on upper cases even if user enters roman values in lower case chars */
+        for (int x = romanNumeral.length() - 1; x >= 0; x--) {
+            char convertToDecimal = romanNumeral.charAt(x);
+
+            switch (convertToDecimal) {
+                case 'M':
+                    decimal = processDecimal(1000, lastNumber, decimal);
+                    lastNumber = 1000;
+                    break;
+
+                case 'D':
+                    decimal = processDecimal(500, lastNumber, decimal);
+                    lastNumber = 500;
+                    break;
+
+                case 'C':
+                    decimal = processDecimal(100, lastNumber, decimal);
+                    lastNumber = 100;
+                    break;
+
+                case 'L':
+                    decimal = processDecimal(50, lastNumber, decimal);
+                    lastNumber = 50;
+                    break;
+
+                case 'X':
+                    decimal = processDecimal(10, lastNumber, decimal);
+                    lastNumber = 10;
+                    break;
+
+                case 'V':
+                    decimal = processDecimal(5, lastNumber, decimal);
+                    lastNumber = 5;
+                    break;
+
+                case 'I':
+                    decimal = processDecimal(1, lastNumber, decimal);
+                    lastNumber = 1;
+                    break;
+            }
+        }
+        return decimal;
+    }
+
+    public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal) {
+            return lastDecimal - decimal;
+        } else {
+            return lastDecimal + decimal;
+        }
+    }
+
+    public boolean isRomanNum(String text) {
+        text = text.toUpperCase();
+        if (text.contains("A") || text.contains("B") || text.contains("E") || text.contains("F") || text.contains("G") || text.contains("H") || text.contains("J") || text.contains("K") || text.contains("N") || text.contains("O") || text.contains("P") || text.contains("Q") || text.contains("R") || text.contains("S") || text.contains("T") || text.contains("U") || text.contains("W") || text.contains("Y") || text.contains("Z") || text.contains(" ") || text.contains(".") || text.contains(",")) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<Pages> trim(XMLEvent event) throws XMLStreamException {
         System.out.println("calling run");
         List<Pages> pageList = new ArrayList<Pages>();
         while (reader.hasNext()) {
@@ -165,17 +244,17 @@ public class XMLTrimmer {
             if (event.isStartElement()) { //first it looks for start elements
                 StartElement se = event.asStartElement();
                 if ("OBJECT".equals(se.getName().getLocalPart())) {
-                    if (!knowPageHeightWidth) {
-                        getPageHeightAndWidth(se);
-                        knowPageHeightWidth = true;
-                    }
+
+                    getPageHeightAndWidth(se);
 
                 } else if ("MAP".equals(se.getName().getLocalPart())) {
                     int pageCount = 0;
                     Pages page = new Pages();
-                    page.acmPageNum = pageCount;
+                    page.acmPageNum = pageList.size();
+                    //pageCount++;
+
+                    //System.out.println("pagecount: " + pageCount);
                     pageList.add(page);
-                    pageCount++;
 
                 } else if ("PARAM".equals(se.getName().getLocalPart())) {
                     if (quarterInch >= 0) {
@@ -186,22 +265,23 @@ public class XMLTrimmer {
                     getWordCoords(se);
 
                     if (inMargin(xOne, yOne, xTwo, yTwo)) { //check to see if in margins, if it is the end tag is written immediately, otherwise nothing is written
-                        Word word = new Word();
+                        Pages.Word word = new Pages.Word();
                         word.xOne = xOne;
                         word.yOne = yOne;
                         word.xTwo = xTwo;
                         word.yTwo = yTwo;
-                        word.pageNum = pageList.size();
+
                         word.text = reader.getElementText();
                         if (pageList.size() == 0) {
 
                             Pages page = new Pages();
-                            page.acmPageNum = 0;
+                            page.acmPageNum = word.acmNum = 0;
                             page.wordsOnPage.add(word);
                             pageList.add(page);
 
-                        } else if (pageList.size()>0) {
+                        } else if (pageList.size() > 0) {
                             Pages lastPage = pageList.get(pageList.size() - 1);
+                            word.acmNum = lastPage.acmPageNum;
                             lastPage.wordsOnPage.add(word);
 
                         }
@@ -219,6 +299,113 @@ public class XMLTrimmer {
         return pageList;
     }
 
+    public ArrayList<NumScheme> findPageNumbers(List<Pages> pagelist) {
+        ArrayList<NumScheme> possPageSeqs = new ArrayList<NumScheme>();
+        for (Pages p : pagelist) {
+            System.out.println("searching pages " + possPageSeqs.size());
+
+            for (Pages.Word w : p.wordsOnPage) {
+                System.out.println("searching words");
+                if (isArabicNum(w.text)) {
+                    System.out.println("found arabic: " + w.text);
+                    handleArabic(possPageSeqs, w);
+                } else if (isRomanNum(w.text)) {
+                    System.out.println("found roman: " + w.text);
+                    handleRoman(possPageSeqs, w);
+
+                }
+
+            }
+            for (NumScheme ns : possPageSeqs) {
+                if (ns.foundElementOnCurrPage==false) {
+                    System.out.println("adding blank");
+                    ns.addBlank();
+                }
+            }
+
+            for (NumScheme num : possPageSeqs) {
+                System.out.println("setting to false");
+                num.foundElementOnCurrPage = false;
+            }
+
+        }
+        System.out.println("poss sequences: " + possPageSeqs.size());
+        return possPageSeqs;
+    }
+
+    public ArrayList<NumScheme> handleArabic(ArrayList<NumScheme> pps, Pages.Word w) {
+
+        NumScheme temp = new NumScheme();
+        System.out.println("calling findsequence");
+        if (findSequence(pps, w) == null) {
+            System.out.println("creating new an");
+            temp.sequence.add(w);
+            temp.foundElementOnCurrPage = true;
+            pps.add(temp);
+        }
+
+        return pps;
+    }
+
+    public NumScheme findSequence(ArrayList<NumScheme> pps, Pages.Word w) {
+        if (pps.size() == 0) {
+            System.out.println("returning null b/c of size");
+            return null;
+        }
+        for (NumScheme ns : pps) {
+            if (!ns.roman) {
+                System.out.println("not roman");
+                int numBlanks = ns.getNumOfBlanks();
+                 System.out.println("word: " + w.text + "number of blanks: " + numBlanks);
+                 int valueOfLast = Integer.valueOf(ns.sequence.get(ns.sequence.size() - (1 + numBlanks)).text);
+
+                //int valueOfLast = Integer.valueOf(ns.sequence.get(ns.sequence.size() - 1).text);
+                System.out.println("value of last: " + valueOfLast);
+                int valueOfCurr = Integer.valueOf(w.text);
+                System.out.println("value of curr: " + valueOfCurr);
+
+                if (valueOfLast + (1+numBlanks) == valueOfCurr) {
+                    ns.foundElementOnCurrPage = true;
+                    ns.arabic = true;
+                    System.out.println("adding to sequence");
+                    ns.sequence.add(w);
+                    return ns;
+                }
+            } else if (!ns.arabic) {
+                int currNumeral = romanToDecimal(w.text);
+                int numBlanks = ns.getNumOfBlanks();
+                 System.out.println("word: " + w.text + "number of blanks: " + numBlanks);
+                int valueOfLast = romanToDecimal(ns.sequence.get(ns.sequence.size() - (1 + numBlanks)).text);
+               // int valueOfLast = romanToDecimal(ns.sequence.get(ns.sequence.size() - 1).text);
+                if (valueOfLast + (1+numBlanks) == currNumeral) {
+                    ns.foundElementOnCurrPage = true;
+                    System.out.println("adding to sequence of rn");
+                    ns.sequence.add(w);
+                    ns.roman = true;
+                    return ns;
+                }
+            }
+        }
+        System.out.println("returning null b/c no schemes fit");
+        return null;
+    }
+
+    public ArrayList<NumScheme> handleRoman(ArrayList<NumScheme> pps, Pages.Word w) {
+
+        NumScheme temp = new NumScheme();
+        System.out.println("calling findsequence");
+        if (findSequence(pps, w) == null) {
+            System.out.println("creating new rn");
+            temp.foundElementOnCurrPage = true;
+            temp.roman = true;
+            temp.sequence.add(w);
+            pps.add(temp);
+        }
+
+        return pps;
+
+    }
+
     public static void main(String args[]) throws IOException, XMLStreamException {
 
         File readFile = new File(args[0]);
@@ -227,65 +414,70 @@ public class XMLTrimmer {
 
         XMLEvent event = null;
 
-        List<Pages> work = xr.run(event);
-        String hope = "";
+        List<Pages> work = xr.trim(event);
+        String output = "";
         System.out.println("number of pages: " + work.size());
         for (Pages p : work) {
-            System.out.println("calling pages.tostring");
+            //System.out.println("calling pages.tostring");
             System.out.println("number of words on page: " + p.wordsOnPage.size());
-            hope = p.toString();
-            System.out.println(hope);
+            output = p.toString();
+            System.out.println(output);
         }
 
     }
 
-    public static class Pages {
+    public static class NumScheme {
 
-        int pageNum = -1;
-        int acmPageNum = -1;
-        List<Word> wordsOnPage = new ArrayList<Word>();
+        int pagesLookedAt;
+        int pageNumsFound;
+        boolean foundElementOnCurrPage = false;
+        boolean arabic;
+        boolean roman;
+        //double ratio = pagesLookedAt / pageNumsFound;
+        List<Pages.Word> sequence = new ArrayList<Pages.Word>();
 
-        public Pages() {
+        public NumScheme() {
         }
 
-        public void addWords(Word w) {
-            wordsOnPage.add(w);
+        public Pages.Word getLast() {
+            return sequence.get(sequence.size() - 1);
         }
 
-        public List<Word> getWords() {
-            return wordsOnPage;
+        public void addBlank() {
+
+            Pages.Word blank = new Pages.Word();
+            blank.isBlank = true;
+            blank.text = "";
+            sequence.add(blank);
+
         }
 
         public String toString() {
-            String stuff = "";
-
-            stuff = "ACM Page Num: " + acmPageNum + "\n";
-            for (Word w : wordsOnPage) {
-
-                stuff = stuff + w.toString();
+            String result = "";
+            for (Pages.Word w : this.sequence) {
+                result = result + w.toString();
             }
-            return stuff;
-        }
-    }
-
-    public static class Word {
-
-        public int pageNum;
-        String text;
-        boolean memOfNumScheme;
-        int xOne; //coordinates of the words
-        int yOne;
-        int xTwo;
-        int yTwo;
-
-        public Word() {
+            return result;
         }
 
-        public String toString() {
-            String stuff = "";
+        public int getNumOfBlanks() {
+            int numOfBlanks = 0;
+            if (this.sequence.size() > 0) {
 
-            stuff = "pagenum = " + pageNum + " " + text + "(" + xOne + "," + yOne + ") (" + xTwo + "," + yTwo + ")\n";
-            return stuff;
+                for (int i = this.sequence.size() - 1; i >= 0; i--) {
+
+                    if (this.sequence.get(i).isBlank) {
+
+                        numOfBlanks++;
+                    } else {
+                        break;
+                    }
+                }
+                return numOfBlanks;
+            } else {
+                return 0;
+            }
         }
+
     }
 }
