a146fd4ef98c1006a2c8d2f7717855cd776b07d4
mzarozinski <MichaelZ@cs.umass.edu>
2014-10-10 16:15:25 -0400

    added SQLException

--- homer/src/main/java/ciir/proteus/users/UserDatabaseFactory.java
+++ homer/src/main/java/ciir/proteus/users/UserDatabaseFactory.java
@@ -1,19 +1,27 @@
 package ciir.proteus.users;
 
 import ciir.proteus.users.impl.H2Database;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.lemurproject.galago.utility.Parameters;
 
 /**
  * @author jfoley.
  */
 public class UserDatabaseFactory {
-  public static UserDatabase instance(Parameters dbp) {
-    String impl = dbp.get("impl", "H2Database");
 
-    if("H2Database".equals(impl)) {
-      return new H2Database(dbp);
+    public static UserDatabase instance(Parameters dbp) throws Exception {
+        String impl = dbp.get("impl", "H2Database");
+
+        if ("H2Database".equals(impl)) {
+            try {
+                return new H2Database(dbp);
+            } catch (SQLException ex) {
+                throw new Exception("Error creating H2 Database");
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown impl=" + impl);
     }
-
-    throw new IllegalArgumentException("Unknown impl="+impl);
-  }
 }
