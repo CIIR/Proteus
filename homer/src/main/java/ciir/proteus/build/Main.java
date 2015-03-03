c210da634103a86b06d68060fad9de3ebf0d600d
mzarozinski <MichaelZ@cs.umass.edu>
2015-03-03 10:58:10 -0500

    Updated so we don't have to call addFunction() for every "function" Proteus provides. It now will register all children of AppFunction

--- homer/src/main/java/ciir/proteus/build/Main.java
+++ homer/src/main/java/ciir/proteus/build/Main.java
@@ -13,15 +13,12 @@ import java.io.File;
  */
 public class Main {
 
-    private static void addFunction(AppFunction fn) {
-        App.appFunctions.put(fn.getName(), fn);
-    }
-
     public static void main(String[] args) throws Exception {
         System.out.println("java.class.path: " + System.getProperty("java.class.path"));
         System.out.println("cwd: " + (new File(".")).getAbsolutePath());
+        // "register" any Proteus functionality (children of AppFunction) that can be called via the command line
+        App.processClassPath("ciir.proteus");
 
-        addFunction(new ProteusMain());
         App.run(args);
     }
 }
