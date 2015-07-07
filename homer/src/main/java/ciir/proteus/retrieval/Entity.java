4eaa104776f2888bf6ba3a81a3c37c3a44fa7285
wem <wem@swarm.cs.umass.edu>
2015-07-07 12:36:26 -0400

    update entity code with new comparison methods

--- homer/src/main/java/ciir/proteus/retrieval/Entity.java
+++ homer/src/main/java/ciir/proteus/retrieval/Entity.java
@@ -1,7 +1,9 @@
 package ciir.proteus.retrieval;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by wem on 6/23/15.
@@ -9,19 +11,41 @@ import java.util.List;
 public class Entity {
 
     String name;
-    List<String> locations;
+    Set<String> aliases;
+    Set<String> locations;
     String languageModel;
     double score;
 
     public Entity(String name){
         this.name = name;
-        this.locations = new ArrayList<String>();
+        this.aliases = new HashSet<String>();
+        this.locations = new HashSet<String>();
         this.languageModel = "";
         this.score = 0;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Set<String> getAliases(){
+        return aliases;
+    }
+
+    public void addAlias(String name){
+        aliases.add(name);
+    }
+
+    public Set<String> getLocations(){
+        return locations;
+    }
+
     public void addLocation(String identifier){
         locations.add(identifier);
+    }
+
+    public String getLanguageModel(){
+        return languageModel;
     }
 
     public void extendLanguageModel(String newText){
@@ -30,6 +54,48 @@ public class Entity {
 
     public void addScore(double score){
         this.score = this.score + score;
+    }
+
+    // compare entities on the basis of the string edit distance of their aliases
+    public int compareAliases(String s) {
+        int minDistance = Integer.MAX_VALUE;
+        for(String alias: aliases){
+            int newDistance = StringUtils.getLevenshteinDistance(alias, s);
+            minDistance = (newDistance < minDistance) ? newDistance : minDistance;
+        }
+        System.out.println("Comparison: " + name);
+        System.out.println("Score: " + minDistance);
+        return minDistance;
+    }
+
+    //TODO finish this code
+    // compare entities on the basis of the string edit distance of their aliases
+    // minDistance returned is for the min distance between to segments
+    // thus "David Johnson" and "David Jonson" would have a distance of 0
+    public int compareAliasesBySegment(String s) {
+        int minDistance = Integer.MAX_VALUE;
+        for(String alias: aliases){
+            int newDistance = StringUtils.getLevenshteinDistance(name, s);
+            minDistance = (newDistance < minDistance) ? newDistance : minDistance;
+        }
+        return minDistance;
+    }
+
+    // compare entities on the basis of the string edit distance of their aliases
+    public int compare(Entity e) {
+        int minDistance = Integer.MAX_VALUE;
+        for(String alias: e.aliases){
+            int newDistance = compareAliases(alias);
+            minDistance = (newDistance < minDistance) ? newDistance : minDistance;
+        }
+        return minDistance;
+    }
+
+    //add the data from an entity to this entity
+    public void addEntity(Entity e){
+        aliases.addAll(e.aliases);
+        locations.addAll(e.locations);
+        extendLanguageModel(e.languageModel);
     }
 
 }
