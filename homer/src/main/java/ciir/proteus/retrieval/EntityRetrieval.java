6c453faf4f48294a8100042991163074c2b96105
wem <wem@swarm.cs.umass.edu>
2015-07-07 12:36:46 -0400

    updated entity retireval

--- homer/src/main/java/ciir/proteus/retrieval/EntityRetrieval.java
+++ homer/src/main/java/ciir/proteus/retrieval/EntityRetrieval.java
@@ -1,6 +1,10 @@
 package ciir.proteus.retrieval;
 
 import ciir.proteus.util.QueryUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.lemurproject.galago.core.parse.Document;
+import org.lemurproject.galago.core.parse.Tag;
+import org.lemurproject.galago.core.parse.TagTokenizer;
 import org.lemurproject.galago.core.retrieval.LocalRetrieval;
 import org.lemurproject.galago.core.retrieval.Retrieval;
 import org.lemurproject.galago.core.retrieval.ScoredDocument;
@@ -8,6 +12,7 @@ import org.lemurproject.galago.core.retrieval.query.Node;
 import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
 import org.lemurproject.galago.utility.Parameters;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -48,37 +53,49 @@ public class EntityRetrieval {
         }
     }
 
-    public List<Entity> buildSortedEntityList(){
-        HashMap <String, Entity> entities= new HashMap<>();
+    public List<Entity> buildSortedEntityList() throws IOException {
+        ArrayList<Entity> entities= new ArrayList<Entity>();
 
         for(ScoredDocument sDoc: resultsList){
             System.out.println(sDoc.documentName);
-            String name = sDoc.documentName.split("\\.")[0];
-            String identifier = sDoc.documentName.split("\\.")[1];
-            if(!entities.containsKey(name)) {
-                Entity e = new Entity(name);
-                entities.put(name, e);
+            String name = sDoc.documentName;
+            Entity e = new Entity(name);
+            TagTokenizer tok = new TagTokenizer();
+            tok.addField("alias");
+            tok.addField("location");
+            Document doc = ret.getDocument(sDoc.documentName, new Document.DocumentComponents(true, true, true));
+            tok.tokenize(doc);
+            for(Tag tag: doc.tags){
+                StringBuilder sb = new StringBuilder();
+                for (int i = tag.begin; i < tag.end; i++) {
+                    sb.append(doc.terms.get(i));
+                }
+                if(tag.name.equals("alias")) e.addAlias(sb.toString());
+                if(tag.name.equals("location")) e.addLocation(sb.toString());
             }
-            entities.get(name).addLocation(identifier);
-            entities.get(name).addScore(sDoc.getScore());
-            entities.get(name).extendLanguageModel(sDoc.toString());
+            e.addScore(sDoc.getScore());
+            e.extendLanguageModel(sDoc.toString());
+            entities.add(e);
 
         }
 
-        TreeMap<Double, List<Entity>> scoreMap = new TreeMap<Double, List<Entity>>(Collections.reverseOrder());
-        for(Entity e: entities.values()) {
-            if(!scoreMap.containsKey(e.score)) scoreMap.put(e.score, new ArrayList<Entity>());
-            scoreMap.get(e.score).add(e);
-        }
-
-        List<Entity> sortedEntities = new ArrayList<Entity>();
-        for(List<Entity> el: scoreMap.values()){
-            for(Entity e: el) sortedEntities.add(e);
-        }
-        return sortedEntities;
+        return entities;
     }
 
-    public List<Entity> search(String rawQuery, int count){
+    //create a new entity from the data of two other entities
+    //the entity takes the name of the first entity
+    public Entity mergeEntities(Entity a, Entity b){
+        Entity merged = new Entity(a.name);
+        merged.aliases.addAll(a.aliases);
+        merged.aliases.addAll(b.aliases);
+        merged.locations.addAll(a.locations);
+        merged.locations.addAll(b.locations);
+        merged.extendLanguageModel(a.languageModel);
+        merged.extendLanguageModel(b.languageModel);
+        return merged;
+    }
+
+    public List<Entity> search(String rawQuery, int count) throws IOException {
         documentSearch(rawQuery, count);
         List<Entity> sortedEntities = buildSortedEntityList();
 
