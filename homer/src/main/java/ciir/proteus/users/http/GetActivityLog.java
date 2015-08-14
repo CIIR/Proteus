d3337193396e93974c23182ed5f5a866edbce262
mzarozinski <MichaelZ@cs.umass.edu>
2015-08-14 13:32:40 -0400

    updated to get all available old logs rather than just todays.

--- homer/src/main/java/ciir/proteus/users/http/GetActivityLog.java
+++ homer/src/main/java/ciir/proteus/users/http/GetActivityLog.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author michaelz.
@@ -22,24 +28,59 @@ public class GetActivityLog extends DBAction {
     @Override
     public Parameters handle(String method, String path, Parameters reqp, HttpServletRequest req) throws HTTPError, DBError, IOException {
 
-        String filePath = reqp.get("file", "proteus-activity.log");
-        //Integer corpusid = reqp.get("corpus", -1);
-        Parameters events = Parameters.create();
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        List<String> fileList = new ArrayList<String>();
+        // code from: http://stackoverflow.com/questions/3008043/list-all-files-from-directories-and-subdirectories-in-java
         try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+            Path startPath = Paths.get("logs");
+            Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir,
+                                                         BasicFileAttributes attrs) {
+                    //System.out.println("Dir: " + dir.toString());
+                    return FileVisitResult.CONTINUE;
+                }
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            events.put("events", sb.toString());
-        } finally {
-            br.close();
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    //System.out.println("File: " + file.toString());
+                    fileList.add(file.toString());
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException e) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        // make sure we're in the correct order
+        Collections.sort(fileList);
+
+        Parameters events = Parameters.create();
+        StringBuilder sb = new StringBuilder();
+
+        for (String fp : fileList){
+
+            BufferedReader br = new BufferedReader(new FileReader(fp));
+            try {
+
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                
+            } finally {
+                br.close();
+            }
+        }
+
+        events.put("events", sb.toString());
         return events;
     }
 }
