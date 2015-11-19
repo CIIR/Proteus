587305aaf44f653d2fc76f6e65181aa9bc22ec60
mzarozinski <MichaelZ@cs.umass.edu>
2015-11-19 09:45:42 -0500

    added logic to get archiveid from rawtei to cut down on unimportant error messages.

--- homer/src/main/java/ciir/proteus/parse/MBTEIParser.java
+++ homer/src/main/java/ciir/proteus/parse/MBTEIParser.java
@@ -81,8 +81,10 @@ public abstract class MBTEIParser extends DocumentStreamParser {
       archiveId = basename.split(".mbtei.gz")[0];
     else if (basename.endsWith(".toktei.gz"))
       archiveId = basename.split(".toktei.gz")[0];
+    else if (basename.endsWith(".rawtei.gz"))
+      archiveId = basename.split(".rawtei.gz")[0];
     else {
-      System.err.println("File extension was expected to be .mbtei.gz or .toktei.gz");
+      System.err.println("File extension was expected to be .rawtei.gz, .mbtei.gz or .toktei.gz");
       archiveId = basename;
     }
     // put it in the metadata if we had to guess it.
