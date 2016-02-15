867d174b8587d19b71bf4fcb1e9161fe0e8bf100
wem <wem@cs.umass.edu>
2016-02-14 21:56:21 -0500

    updated entity recorder to use regexes rather than an external named entity recognizer.

--- homer/src/main/java/ciir/proteus/parse/NamedEntityRecorder.java
+++ homer/src/main/java/ciir/proteus/parse/NamedEntityRecorder.java
@@ -47,49 +47,13 @@ public class NamedEntityRecorder {
         }
 
         boolean isNew = true;
-        //for(String entity: entityTypes) {
-        //    System.out.println(entity);
-        //}
         for(String entity: entityTypes){
             StringBuilder sb = new StringBuilder();
             TagTokenizer tok = new TagTokenizer();
             tok.addField(entity);
             tok.tokenize(doc);
-            HashSet<String> names = new HashSet<String>();
-            HashMap<String, Integer> nameCounts = new HashMap<String, Integer>();
-            int totalCount = 0;
-            int uniqueCount = 0;
-            for(Tag tag: doc.tags) {
-                StringBuilder nb = new StringBuilder();
-                for (int i = tag.begin; i < tag.end; i++) {
-                    nb.append(doc.terms.get(i) + " ");
-                }
-                if(!(nb.lastIndexOf(" ")==-1)){
-                    nb.deleteCharAt(nb.lastIndexOf(" "));
-                }
-                String name = nb.toString();
-
-                Pattern p = Pattern.compile("(\\d{4}|\\d{3})");
-                Matcher m = p.matcher(name);
-
-                List<String> years = new ArrayList<String>();
-                while (m.find()) {
-                    years.add(m.group());
-                }
-                if(years.size() > 0) {
-                    for(String year: years)
-                        sb.append(year);
-                        sb.append(",");
-                        sb.append(doc.name);
-                        sb.append("\n");
-                }
-                else if(entity.equals("location")) {
-                    sb.append(name);
-                    sb.append(",");
-                    sb.append(doc.name);
-                    sb.append("\n");
-                }
-            }
+            if(entity.equals("date")) sb = findLocations(doc,sb);
+            else if(entity.equals("location")) sb = findLocations(doc,sb);
 
             String docName = doc.name.split("_")[0];
             String outputName = writeOutLocation+entity+"/"+docName+".txt";
@@ -100,5 +64,55 @@ public class NamedEntityRecorder {
             bw.close();
 
         }
+    }
+
+    public StringBuilder findLocations(Document doc, StringBuilder sb){
+        for(Tag tag: doc.tags) {
+            StringBuilder nb = new StringBuilder();
+            for (int i = tag.begin; i < tag.end; i++) {
+                nb.append(doc.terms.get(i) + " ");
+            }
+            if(!(nb.lastIndexOf(" ")==-1)){
+                nb.deleteCharAt(nb.lastIndexOf(" "));
+            }
+            String name = nb.toString();
+            sb.append(name);
+            sb.append(",");
+            sb.append(doc.name);
+            sb.append("\n");
+        }
+        return sb;
+    }
+
+    public StringBuilder findDates(Document doc, StringBuilder sb){
+        for(String term: doc.terms){
+            //treat dates anything that is either a three or four
+            //digit number, or two three or four digit numbers linked
+            //with a dash
+            String[] terms = term.split("-");
+            ArrayList<String> candidates = new ArrayList<String>();
+            for(int i = 0; i<terms.length;i++){
+                if(terms[i].length() == 3 || terms[i].length() == 4){
+                    candidates.add(terms[i]);
+                }
+            }
+            Pattern p = Pattern.compile("(\\d{4}|\\d{3})");
+            List<String> years = new ArrayList<String>();
+            for(String candidate: candidates) {
+                Matcher m = p.matcher(candidate);
+                while (m.find()) {
+                    years.add(m.group());
+                }
+            }
+            if(years.size() > 0) {
+                for(String year: years)
+                    sb.append(year);
+                sb.append(",");
+                sb.append(doc.name);
+                sb.append("\n");
+            }
+
+        }
+        return sb;
     }
 }
