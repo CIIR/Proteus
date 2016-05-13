fbd7024a630d27ca0f0e62f56b14f4610a544c5a
mzarozinski <MichaelZ@cs.umass.edu>
2016-05-13 09:07:21 -0400

    corrected expected/actual in asserts

--- homer/src/test/java/ciir/proteus/util/QueryUtilTest.java
+++ homer/src/test/java/ciir/proteus/util/QueryUtilTest.java
@@ -22,7 +22,7 @@ public class QueryUtilTest {
     // simple query: max d cat
     Node query = StructuredQuery.parse("#combine( #text:max() #text:d() #text:cat() )");
     terms = QueryUtil.queryTerms(query);
-    assertEquals(terms.size(), 3);
+    assertEquals(3, terms.size());
     assertTrue(terms.contains("max"));
     assertTrue(terms.contains("d"));
     assertTrue(terms.contains("cat"));
@@ -31,19 +31,19 @@ public class QueryUtilTest {
     // "pine tree shilling"
     query = StructuredQuery.parse("#combine( #ordered:1( #text:pine() #text:tree() #text:shilling() ) )");
     terms = QueryUtil.queryTerms(query);
-    assertEquals(terms.size(), 1);
+    assertEquals(1, terms.size());
     assertTrue(terms.contains("pine tree shilling"));
 
     // currently, we assume all phrases are ordered window 1
     query = StructuredQuery.parse("#combine( #ordered:3( #text:pine() #text:tree() #text:shilling() ) )");
     terms = QueryUtil.queryTerms(query);
-    assertEquals(terms.size(), 1);
+    assertEquals(1, terms.size());
     assertTrue(terms.contains("pine tree shilling"));
 
     // treat unordered as a regular query
     query = StructuredQuery.parse("#combine( #unordered:2( #text:pine() #text:tree() #text:shilling() ) )");
     terms = QueryUtil.queryTerms(query);
-    assertEquals(terms.size(), 3);
+    assertEquals(3, terms.size());
     assertTrue(terms.contains("pine"));
     assertTrue(terms.contains("tree"));
     assertTrue(terms.contains("shilling"));
@@ -52,16 +52,29 @@ public class QueryUtilTest {
     // organization:"pan american union"
     query = StructuredQuery.parse("#combine( #inside( #ordered:1( #text:pan() #text:american() #text:union() ) #field:organization() ) )");
     terms = QueryUtil.queryTerms(query);
-    assertEquals(terms.size(), 1);
+    assertEquals(1, terms.size());
     assertTrue(terms.contains("pan american union"));
 
     // person:"charles" location:"new england"
     query = StructuredQuery.parse("#combine( #inside( #text:charles() #field:person() ) #inside( #ordered:1( #text:new() #text:england() ) #field:location() ) )");
-
-    Node tq = SimpleQuery.parseTree("person:\"charles\" location:\"new england\"");
     terms = QueryUtil.queryTerms(query);
-    terms = QueryUtil.queryTerms(tq);
-    assertEquals(terms.size(), 2);
+    assertEquals(2, terms.size());
+    assertTrue(terms.contains("charles"));
+    assertTrue(terms.contains("new england"));
+
+    query = StructuredQuery.parse("#combine:w=1.0( #extents:charles:part=field.krovetz.person() )");
+    terms = QueryUtil.queryTerms(query);
+    assertEquals(1, terms.size());
+    assertTrue(terms.contains("charles"));
+
+    query = StructuredQuery.parse("#combine:w=1.0( #inside( #ordered:1( #extents:new:part=postings.krovetz() #extents:england:part=postings.krovetz() ) #extents:location:part=extents() ) )");
+    terms = QueryUtil.queryTerms(query);
+    assertEquals(1, terms.size());
+    assertTrue(terms.contains("new england"));
+
+    query = StructuredQuery.parse("#combine:w=1.0( #extents:charles:part=field.krovetz.person() #inside( #ordered:1( #extents:new:part=postings.krovetz() #extents:england:part=postings.krovetz() ) #extents:location:part=extents() ) )");
+    terms = QueryUtil.queryTerms(query);
+    assertEquals(2, terms.size());
     assertTrue(terms.contains("charles"));
     assertTrue(terms.contains("new england"));
 
