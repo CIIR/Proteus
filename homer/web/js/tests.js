584fd15b6ab5182129644468f1377185f8eb676a
mzarozinski <MichaelZ@cs.umass.edu>
2016-08-25 08:07:28 -0400

    fixed assert param order assert.equal(actual, expected)

--- homer/web/js/tests.js
+++ homer/web/js/tests.js
@@ -3,33 +3,33 @@
  */
 
 // override alert so this script can run without supervision.
-function alert(msg){
+function alert(msg) {
   console.log(msg);
 }
 
-QUnit.module( "Utilities" );
+QUnit.module("Utilities");
 
-QUnit.test( "JQuery Escape", function( assert ) {
+QUnit.test("JQuery Escape", function (assert) {
   var id = 'NothingSpecial';
   var newid = jqEsc(id)
-  assert.equal( id, newid);
+  assert.equal(newid, id);
 
   id = 'i.have.dots';
   newid = jqEsc(id)
-  assert.equal( "i\\.have\\.dots", newid);
+  assert.equal(newid, "i\\.have\\.dots");
 
   id = 'all.special:chars[]';
   newid = jqEsc(id)
-  assert.equal( "all\\.special\\:chars\\[\\]", newid);
+  assert.equal(newid, "all\\.special\\:chars\\[\\]");
 });
 
-QUnit.test( "Parse Internet Archive ID", function( assert ) {
+QUnit.test("Parse Internet Archive ID", function (assert) {
   var pageid = 'id_123';
   var obj = parsePageID(pageid);
   assert.equal(obj.id, "id");
-  assert.equal(obj.page,"123");
-  assert.equal(obj.page,123);
-  assert.equal(obj.note,"");
+  assert.equal(obj.page, "123");
+  assert.equal(obj.page, 123);
+  assert.equal(obj.note, "");
 
   // make sure cache was updated and we use it
   assert.equal(parsedPageCache.size, 1);
@@ -41,257 +41,257 @@ QUnit.test( "Parse Internet Archive ID", function( assert ) {
   pageid = 'poems___00wott_191';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "poems___00wott");
-  assert.equal(obj.page,"191");
-  assert.equal(obj.note,"");
+  assert.equal(obj.page, "191");
+  assert.equal(obj.note, "");
 
   // no page number
   pageid = 'archiveid';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "archiveid");
-  assert.equal(obj.page,"");
-  assert.equal(obj.note,"");
+  assert.equal(obj.page, "");
+  assert.equal(obj.note, "");
 
   // NaN page number
   pageid = 'archiveid_foo';
   obj = parsePageID(pageid);
   assert.equal(obj.id, pageid);
   assert.equal(obj.page, "");
-  assert.equal(obj.note,"");
+  assert.equal(obj.note, "");
 
   pageid = 'archiveid_123_foo';
   obj = parsePageID(pageid);
   assert.equal(obj.id, pageid);
   assert.equal(obj.page, "");
-  assert.equal(obj.note,"");
+  assert.equal(obj.note, "");
 
   pageid = 'archiveid_123xyz';
   obj = parsePageID(pageid);
   assert.equal(obj.id, pageid);
   assert.equal(obj.page, "");
-  assert.equal(obj.note,"");
+  assert.equal(obj.note, "");
 
   // id is a number
   pageid = '123_456';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "123");
   assert.equal(obj.page, "456");
-  assert.equal(obj.note,"");
+  assert.equal(obj.note, "");
 
   pageid = '123';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "123");
   assert.equal(obj.page, "");
-  assert.equal(obj.note,"");
+  assert.equal(obj.note, "");
 
   // test note numbers
   pageid = 'archiveid_123_456';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "archiveid");
   assert.equal(obj.page, "123");
-  assert.equal(obj.note,"456");
+  assert.equal(obj.note, "456");
 
   pageid = 'poems___00wott_191_23';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "poems___00wott");
-  assert.equal(obj.page,"191");
-  assert.equal(obj.note,"23");
+  assert.equal(obj.page, "191");
+  assert.equal(obj.note, "23");
 
   pageid = '123_456_789';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "123");
-  assert.equal(obj.page,"456");
-  assert.equal(obj.note,"789");
+  assert.equal(obj.page, "456");
+  assert.equal(obj.note, "789");
 
   pageid = '123_456_789_101112';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "123_456");
-  assert.equal(obj.page,"789");
-  assert.equal(obj.note,"101112");
+  assert.equal(obj.page, "789");
+  assert.equal(obj.note, "101112");
 
   pageid = '123_456_789_101112x';
   obj = parsePageID(pageid);
   assert.equal(obj.id, "123_456_789_101112x");
-  assert.equal(obj.page,"");
-  assert.equal(obj.note,"");
+  assert.equal(obj.page, "");
+  assert.equal(obj.note, "");
 
 });
 
-QUnit.test( "Capitalize Each Word", function( assert ) {
+QUnit.test("Capitalize Each Word", function (assert) {
   var sentence = 'one two three';
-  assert.equal("One Two Three", sentence.capitalizeEachWord());
+  assert.equal(sentence.capitalizeEachWord(), "One Two Three");
 
   sentence = 'onE two tHree';
-  assert.equal("OnE Two THree", sentence.capitalizeEachWord());
+  assert.equal(sentence.capitalizeEachWord(), "OnE Two THree");
 
 });
 
-QUnit.test( "Trim", function( assert ) {
+QUnit.test("Trim", function (assert) {
   var sentence = ' start';
-  assert.equal("start", sentence.trim());
+  assert.equal(sentence.trim(), "start");
 
   sentence = 'end ';
-  assert.equal("end", sentence.trim());
+  assert.equal(sentence.trim(), "end");
 
   sentence = '     start'; // multiple spaces
-  assert.equal("start", sentence.trim());
+  assert.equal(sentence.trim(), "start");
 
   sentence = 'end    ';
-  assert.equal("end", sentence.trim());
+  assert.equal(sentence.trim(), "end");
 
   sentence = ' both ';
-  assert.equal("both", sentence.trim());
+  assert.equal(sentence.trim(), "both");
 
   sentence = '   both   ';
-  assert.equal("both", sentence.trim());
+  assert.equal(sentence.trim(), "both");
 
   sentence = '\tstartTab';
-  assert.equal("startTab", sentence.trim());
+  assert.equal(sentence.trim(), "startTab");
 
   sentence = 'endTab\t';
-  assert.equal("endTab", sentence.trim());
+  assert.equal(sentence.trim(), "endTab");
 
   sentence = ' \t  mixed  \t '; // mixed tabs and spaces
-  assert.equal("mixed", sentence.trim());
+  assert.equal(sentence.trim(), "mixed");
 
 });
 
-QUnit.test( "Is Blank?", function( assert ) {
+QUnit.test("Is Blank?", function (assert) {
   var sentence = ' start';
 
   assert.notOk(isBlank(sentence));
 
   sentence = 'end    ';
-  assert.equal(false, isBlank(sentence));
+  assert.equal(isBlank(sentence), false);
 
   sentence = '     ';
-  assert.equal(true, isBlank(sentence));
+  assert.equal(isBlank(sentence), true);
 
   sentence = '\t';
-  assert.equal(true, isBlank(sentence));
+  assert.equal(isBlank(sentence), true);
 
   sentence = '  \t   ';
-  assert.equal(true, isBlank(sentence));
+  assert.equal(isBlank(sentence), true);
 
   var empty;
-  assert.equal(true, isBlank(empty));
+  assert.equal(isBlank(empty), true);
 
-  assert.equal(true, isBlank(undefined));
+  assert.equal(isBlank(undefined), true);
 
 });
 
-QUnit.test( "Guess Kind", function( assert ) {
+QUnit.test("Guess Kind", function (assert) {
 
   var id = 123;
-  assert.equal('article', guessKind(id));
+  assert.equal(guessKind(id), 'article');
 
   var id2 = '123';
-  assert.equal('article', guessKind(id2));
+  assert.equal(guessKind(id2), 'article');
 
   var id3 = "abc";
-  assert.equal('ia-books', guessKind(id3));
+  assert.equal(guessKind(id3), 'ia-books');
 
   id3 = "abc_123";
-  assert.equal('ia-pages', guessKind(id3));
+  assert.equal(guessKind(id3), 'ia-pages');
 
   id3 = "abc_123_456";
-  assert.equal('ia-notes', guessKind(id3));
+  assert.equal(guessKind(id3), 'ia-notes');
 
   id3 = "abc_123_456_789";
-  assert.equal(undefined, guessKind(id3));
+  assert.equal(guessKind(id3), undefined);
 
 });
 
 var invalidID = 'dyescoinencXXyclop00dyejiala';
 var validID = 'dyescoinencyclop00dyejiala';
 
-QUnit.test( "getInternetArchiveJS - Invalid ID", function( assert ) {
+QUnit.test("getInternetArchiveJS - Invalid ID", function (assert) {
 
   var done = assert.async();
 
   // First call with invalid ID so book reader will be undefined.
-  getInternetArchiveJS(invalidID, function(){
-    assert.equal(true, _.isUndefined(getBookReader()));
+  getInternetArchiveJS(invalidID, function () {
+    assert.equal(_.isUndefined(getBookReader()), true);
     done();
   });
 
 });
 
-QUnit.test( "getInternetArchiveJS - valid ID", function( assert ) {
+QUnit.test("getInternetArchiveJS - valid ID", function (assert) {
 
   var done = assert.async();
 
   // valid archive id
-  getInternetArchiveJS(validID, function(){
+  getInternetArchiveJS(validID, function () {
     var tmpBR = getBookReader();
-    assert.equal(false, _.isUndefined(tmpBR));
-    assert.equal(validID, tmpBR.getBookID());
+    assert.equal(_.isUndefined(tmpBR), false);
+    assert.equal(tmpBR.getBookID(), validID);
     done();
   });
 
 });
 
-QUnit.test( "getInternetArchiveJS - book id mismatch", function( assert ) {
+QUnit.test("getInternetArchiveJS - book id mismatch", function (assert) {
 
   var done = assert.async();
 
   // invalid archive id - book reader should be the prior one
-  getInternetArchiveJS(invalidID, function(){
+  getInternetArchiveJS(invalidID, function () {
     var tmpBR = getBookReader();
-    assert.equal(false, _.isUndefined(tmpBR));
-    assert.equal(validID, tmpBR.getBookID());
+    assert.equal(_.isUndefined(tmpBR), false);
+    assert.equal(tmpBR.getBookID(), validID);
     done();
   });
 
 });
 
 
-QUnit.test( "getInternetArchiveMetadata - Invalid ID", function( assert ) {
+QUnit.test("getInternetArchiveMetadata - Invalid ID", function (assert) {
 
   var done = assert.async();
 
   // First call with invalid ID so book reader will be undefined.
   var data = {};
-  assert.equal(true, _.isUndefined(data.metadata));
-  getInternetArchiveMetadata(invalidID, data, function(){
-    assert.equal(true, _.isUndefined(data.metadata));
+  assert.equal(_.isUndefined(data.metadata), true);
+  getInternetArchiveMetadata(invalidID, data, function () {
+    assert.equal(_.isUndefined(data.metadata), true);
     done();
   });
 
 });
 
-QUnit.test( "getInternetArchiveMetadata - valid ID", function( assert ) {
+QUnit.test("getInternetArchiveMetadata - valid ID", function (assert) {
 
   var done = assert.async();
 
   // valid archive id
   var data = {};
-  assert.equal(true, _.isUndefined(data.metadata));
-  getInternetArchiveMetadata(validID, data, function(){
-    assert.equal(false, _.isUndefined(data.metadata));
+  assert.equal(_.isUndefined(data.metadata), true);
+  getInternetArchiveMetadata(validID, data, function () {
+    assert.equal(_.isUndefined(data.metadata), false);
     assert.ok(_.size(data.metadata) > 0);
-    assert.equal(true, _.isUndefined(data.metadata.cached));
+    assert.equal(_.isUndefined(data.metadata.cached), true);
     done();
   });
 
 });
 
-QUnit.test( "getInternetArchiveMetadata - cached", function( assert ) {
+QUnit.test("getInternetArchiveMetadata - cached", function (assert) {
 
   var done = assert.async();
 
   // valid archive id - should use cached value
   var data = {};
-  assert.equal(true, _.isUndefined(data.metadata));
-  getInternetArchiveMetadata(validID, data, function(){
-    assert.equal(false, _.isUndefined(data.metadata));
+  assert.equal(_.isUndefined(data.metadata), true);
+  getInternetArchiveMetadata(validID, data, function () {
+    assert.equal(_.isUndefined(data.metadata), false);
     assert.ok(_.size(data.metadata) > 0);
-    assert.equal(false, _.isUndefined(data.metadata.cached));
+    assert.equal(_.isUndefined(data.metadata.cached), false);
     done();
   });
 });
 
 
-QUnit.test( "naturalSortByField", function( assert ) {
+QUnit.test("naturalSortByField", function (assert) {
 
   var records = [
     {key: 'z', value: 1},
@@ -301,7 +301,7 @@ QUnit.test( "naturalSortByField", function( assert ) {
     {key: 'c', value: 0}
   ];
 
-  var sorted =  records.naturalSortByField("key");
+  var sorted = records.naturalSortByField("key");
 
   assert.equal(records[2], sorted[0]);
   assert.equal(records[1], sorted[1]);
@@ -309,7 +309,7 @@ QUnit.test( "naturalSortByField", function( assert ) {
   assert.equal(records[3], sorted[3]);
   assert.equal(records[0], sorted[4]);
 
-  sorted =  records.naturalSortByField("value");
+  sorted = records.naturalSortByField("value");
 
   assert.equal(records[4], sorted[0]);
   assert.equal(records[0], sorted[1]);
